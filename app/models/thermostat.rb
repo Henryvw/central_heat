dac7cf87330d5956d0fda229e3fdb0123810d8c3
Henry van Wagenberg <henryvw1@gmail.com>
2020-04-27 12:29:51 +0200

    Add random household_token generation

--- app/models/thermostat.rb
+++ app/models/thermostat.rb
@@ -1,3 +1,14 @@
 class Thermostat < ApplicationRecord
   has_many :readings
+  before_create :set_auth_household_token
+
+  private
+  def set_auth_household_token
+    return if household_token.present?
+    self.household_token = generate_household_token
+  end
+
+  def generate_household_token
+    SecureRandom.uuid.gsub(/\-/,'')
+  end
 end
--- spec/models/thermostat_spec.rb
+++ spec/models/thermostat_spec.rb
@@ -12,4 +12,10 @@ RSpec.describe Thermostat, type: :model do
   it 'finds and returns a specific reading associated with it' do
     expect(thermostat.readings.find(id=reading.id)).to eq(reading)
   end
+
+  it 'generates and returns a household_token' do
+    allow(SecureRandom).to receive(:uuid).and_return('6b8b6c369061498786818825d49ebf06')
+    new_thermostat = FactoryBot.create(:thermostat)
+    expect(new_thermostat.household_token).to eq ('6b8b6c369061498786818825d49ebf06')
+  end
 end
