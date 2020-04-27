fcc3cb9fe3215ed67f057615be5d132a12590bc7
Henry van Wagenberg <henryvw1@gmail.com>
2020-04-27 15:03:00 +0200

    Add sidekiq worker for asynch POST reading

--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -2,10 +2,23 @@ module Api
   module V1
     class ReadingsController < Api::V1::BaseController
       def create
+        if ReadingWorker.perform_async(reading_params.to_h)
+          head :accepted
+        else
+          head :bad_request
+        end
       end
 
       def show
         @reading = Reading.find(params[:id])
+      end
+
+      private
+      def reading_params
+        params.permit(:thermostat_id,
+                      :temperature,
+                      :humidity,
+                      :battery_charge)
       end
     end
   end
---
+++ app/workers/reading_worker.rb
@@ -0,0 +1,15 @@
+class ReadingWorker
+  include Sidekiq::Worker
+
+  sidekiq_options retry: false
+
+  def perform(new_reading_hash)
+    readings = [ new_reading = Reading.new(new_reading_hash) ]
+    puts "Sidekiq worker creating new reading"
+
+    if new_reading.save
+    else
+      puts errors: new_reading.errors
+    end
+  end
+end
